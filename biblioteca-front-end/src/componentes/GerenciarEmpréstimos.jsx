fe6cbfdd9408e8f86f5f317603cfea2b82f32d0c
Kevin <kevin.a.ortega131@gmail.com>
2024-11-19 22:31:33 -0300

    Alterações no front-end de GerenciarEmprestimos

--- biblioteca-front-end/src/componentes/GerenciarEmpréstimos.jsx
+++ biblioteca-front-end/src/componentes/GerenciarEmpréstimos.jsx
@@ -20,7 +20,12 @@ const GerenciarEmprestimos = () => {
     const [emprestimoADeletar, setEmprestimoADeletar] = useState(null);
     const [alunos, setAlunos] = useState([]);
     const [exemplares, setExemplares] = useState([]); 
-    const [selectedExemplares, setSelectedExemplares] = useState([{ id: '' }]);
+    const [selectedExemplares, setSelectedExemplares] = useState([{ 
+        id: 0, 
+        codigo: '', 
+        titulo: { id: 0, nome: '', genero: { id: 0, genero: '' }, autores: [] }, 
+        status: ''
+    }]);
     const alunoServico = new AlunoServico();
     const exemplarServico = new ExemplarServico();
     const emprestimoServico = new EmprestimoServico();
@@ -132,7 +137,12 @@ const GerenciarEmprestimos = () => {
     };
     
     const handleAddExemplarField = () => {
-        setSelectedExemplares([...selectedExemplares, { id: 0 }]);
+        setSelectedExemplares([...selectedExemplares, { 
+            id: 0, 
+            codigo: '', 
+            titulo: { id: 0, nome: '', genero: { id: 0, genero: '' }, autores: [] }, 
+            status: ''
+        }]);
     };
 
     const handleRemoveExemplarField = (index) => {
@@ -180,24 +190,38 @@ const GerenciarEmprestimos = () => {
                 setNovoEmprestimo({ ...novoEmprestimo, aluno: alunoSelecionado });
             }
         } else if (field === 'exemplar') {
-            const updatedExemplares = [...selectedExemplares];
-            updatedExemplares[index] = { id: parseInt(value) };
-            setSelectedExemplares(updatedExemplares);
-            setNovoEmprestimo({ ...novoEmprestimo, exemplares: updatedExemplares });
+            const exemplarSelecionado = exemplares.find(exemplar => exemplar.id === parseInt(value));
+            
+            if (exemplarSelecionado && !selectedExemplares.some(selected => selected.id === exemplarSelecionado.id)) {
+                const updatedExemplares = [...selectedExemplares];
+               
+                if (index !== null) {
+                    updatedExemplares[index] = exemplarSelecionado; 
+                } else {
+                    updatedExemplares.push(exemplarSelecionado); 
+                }
+    
+                setSelectedExemplares(updatedExemplares);
+                const updatedDisponiveis = exemplares.filter(exemplar => exemplar.id !== exemplarSelecionado.id);
+
+                setNovoEmprestimo({ ...novoEmprestimo, exemplares: updatedExemplares });
+                setExemplares(updatedDisponiveis);
+            }
         } else if (field === 'dataEmprestimo') {
             if (value.length === 10) {
-                setNovoEmprestimo({ ...novoEmprestimo, dataEmprestimo: value })    
+                setNovoEmprestimo({ ...novoEmprestimo, dataEmprestimo: value });
             }
         } else if (field === 'dataPrazo') {
             if (value.length === 10) {
-                setNovoEmprestimo({ ...novoEmprestimo, dataPrazo: value })
+                setNovoEmprestimo({ ...novoEmprestimo, dataPrazo: value });
             }
         } else {
             setNovoEmprestimo({ ...novoEmprestimo, [field]: value });
         }
+        
         validateField(field, value);
     };
-
+        
     const handleDeleteEmprestimo = (index) => {
         setEmprestimoADeletar(index);
         setDeleteConfirmationModalIsOpen(true);
@@ -402,13 +426,13 @@ const GerenciarEmprestimos = () => {
                                     <td className="table-row-text">
                                         <button className="edit-button" onClick={() => { handleEditEmprestimo(index); }}>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                <path d="M16 5L19 8M20.385 6.585C20.7788 6.19115 21.0001 5.65698 21.0001 5.1C21.0001 4.54302 20.7788 4.00885 20.385 3.615C19.9912 3.22115 19.457 2.99989 18.9 2.99989C18.343 2.99989 17.8088 3.22115 17.415 3.615L9 12V15H12L20.385 6.585Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                                <path d="M16 5L19 8M20.385 6.585C20.7788 6.19115 21.0001 5.65698 21.0001 5.1C21.0001 4.54302 20.7788 4.00885 20.385 3.615C19.9912 3.22115 19.457 2.99989 18.9 2.99989C18.343 2.99989 17.8088 3.22115 17.415 3.615L9 12V15H12L20.385 6.585Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                             </svg>
                                         </button>
                                         <button className="delete-button" onClick={() => handleDeleteEmprestimo(index)}>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                <path d="M4 7H20M10 11V17M14 11V17M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M4 7H20M10 11V17M14 11V17M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                             </svg>
                                         </button>
                                     </td>
@@ -434,9 +458,10 @@ const GerenciarEmprestimos = () => {
                                         onChange={(e) => handleChange('exemplar', e.target.value, index)}
                                     >
                                         <option value="0">Selecione um Exemplar</option>
-                                        {exemplares.map((exemplar) => (
-                                            <option key={exemplar.id} value={exemplar.id}>
-                                                {exemplar.titulo.nome}
+                                        {[...exemplares, ...novoEmprestimo.exemplares]
+                                        .map((exemplarOption) => (
+                                            <option key={exemplarOption.id} value={exemplarOption.id}>
+                                                {exemplarOption.titulo.nome}
                                             </option>
                                         ))}
                                     </select>
